5a89b885e8c088581ed1cf5f463b6e82d07dceec
ardhikarn <ardhikarn@gmail.com>
2020-08-23 18:50:08 +0700

    nav side menu

--- src/components/_base/NavSide.vue
+++ src/components/_base/NavSide.vue
@@ -1,15 +1,17 @@
 <template>
-  <div vertical class>
-    <div class="my-4">
-      <img src="../../assets/img-nav/fork.png" alt="Menu" />
+  <b-col xl="1" lg="1" md="1" class="navside px-1">
+    <div vertical align="center">
+      <div class="my-4">
+        <img src="../../assets/img-nav/fork.png" alt="Menu" />
+      </div>
+      <div class="my-5">
+        <img src="../../assets/img-nav/clipboard.png" alt="History" />
+      </div>
+      <div class="my-4">
+        <img src="../../assets/img-nav/add.png" alt="Add Menu" />
+      </div>
     </div>
-    <div class="my-5">
-      <img src="../../assets/img-nav/clipboard.png" alt="History" />
-    </div>
-    <div class="my-4">
-      <img src="../../assets/img-nav/add.png" alt="Add Menu" />
-    </div>
-  </div>
+  </b-col>
 </template>
 
 <script>
@@ -18,5 +20,4 @@ export default {
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
