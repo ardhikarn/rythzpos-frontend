6d04d785bedd271a450c115f5f1c01d57209bbe6
ardhikarn <ardhikarn@gmail.com>
2020-08-30 22:17:00 +0700

    add component

--- src/components/_base/NavSide.vue
+++ src/components/_base/NavSide.vue
@@ -1,16 +1,29 @@
-<!--<template>
-  <b-col xl="1" lg="1" md="1" class="navside px-1">
-    <div vertical align="center">
-      <div class="my-4">
-        <img src="../../assets/img-nav/fork.png" alt="Menu" />
-      </div>
-      <div class="my-5">
-        <img src="../../assets/img-nav/clipboard.png" alt="History" />
-      </div>
-      <div class="my-4">
-        <img src="../../assets/img-nav/add.png" alt="Add Menu" />
-      </div>
-    </div>
+<template>
+  <b-col sm="1" cols="2" class="align-self-center py-4">
+    <b-button v-b-toggle.my-sidebar class="fas fa-bars"></b-button>
+    <b-sidebar id="my-sidebar" shadow>
+      <b-container fluid>
+        <b-row>
+          <b-col cols="12">
+            <img src="../../assets/img-nav/fork.png" alt="Menu" class="my-5" />
+          </b-col>
+          <b-col cols="12">
+            <a href="/history">
+              <img src="../../assets/img-nav/clipboard.png" alt="History" class="my-5" />
+            </a>
+          </b-col>
+
+          <b-col cols="12">
+            <img
+              src="../../assets/img-nav/add.png"
+              alt="Add Menu"
+              class="my-5"
+              @click="showModal()"
+            />
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-sidebar>
   </b-col>
 </template>
 
@@ -19,5 +32,3 @@ export default {
   name: 'NavSide'
 }
 </script>
-
-<style scoped></style> -->
